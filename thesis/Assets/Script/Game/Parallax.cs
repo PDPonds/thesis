5a43725c123b8d37cf3346b1fdb27b7de476400a
Po2s <pondandmeen@gmail.com>
2024-03-02 15:40:42 +0700

    Y Parallax

--- thesis/Assets/Script/Game/Parallax.cs
+++ thesis/Assets/Script/Game/Parallax.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    float length, startPos;
+    float length, startPos , startY;
     public GameObject target;
-    public float parallaxEffect;
+    public float parallaxXEffect , parallaxYEffect;
 
     void Start()
     {
         startPos = transform.position.x;
+        startY = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void FixedUpdate()
     {
-        float temp = (target.transform.position.x * (1 - parallaxEffect));
-        float dist = (target.transform.position.x * parallaxEffect);
+        float temp = (target.transform.position.x * (1 - parallaxXEffect));
+        float dist = (target.transform.position.x * parallaxXEffect);
+        float yPos = (target.transform.position.y * parallaxYEffect);
 
-        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startPos + dist, startY - yPos, transform.position.z);
         if (temp > startPos + length) startPos += length;
         else if (temp < startPos - length) startPos -= length;
     }
