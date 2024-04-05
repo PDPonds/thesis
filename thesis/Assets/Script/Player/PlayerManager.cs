ac98156e91013f363a7cdb646e2aa338791aced5
Po2s <pondandmeen@gmail.com>
2024-04-05 22:38:02 +0700

    Player Move

--- thesis/Assets/Script/Player/PlayerManager.cs
+++ thesis/Assets/Script/Player/PlayerManager.cs
@@ -183,6 +183,53 @@ public class PlayerManager : MonoBehaviour
 
         #endregion
 
+        #region Move
+        if (currentState != revive && !isDead)
+        {
+            Vector3 centerPoint = GameManager.Instance.CenterPoint.position;
+
+            if (GameManager.Instance.state == GameState.BossFight &&
+                GameManager.Instance.curBoss != null && GameManager.Instance.curBoss.activeSelf
+                && UIManager.Instance.isHorizontalMove)
+            {
+                float inputValue = leftInput - rightInput;
+                curMoveX = curMoveX - inputValue * speed * Time.deltaTime;
+
+                if (curMoveX > maxMoveX) curMoveX = maxMoveX;
+                if (curMoveX < minMoveX) curMoveX = minMoveX;
+
+                Vector2 targetPos = new Vector2(centerPoint.x + curMoveX, transform.position.y);
+
+                transform.position = targetPos;
+            }
+            else if (GameManager.Instance.state == GameState.BeforeGameStart)
+            {
+                speed = GameManager.Instance.currentSpeed;
+                Vector2 targetPos = new Vector2(centerPoint.x, transform.position.y);
+                transform.position = Vector3.MoveTowards(transform.position,
+                    targetPos, speed * Time.deltaTime);
+            }
+            else
+            {
+
+                speed = GameManager.Instance.currentSpeed;
+                Vector2 targetPos = new Vector2(centerPoint.x, transform.position.y);
+                if (transform.position.x < targetPos.x)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position,
+                    targetPos, speed * 1.5f * Time.deltaTime);
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position,
+                    targetPos, speed * Time.deltaTime);
+                }
+
+                curMoveX = .5f;
+            }
+        }
+        #endregion
+
         #region Counter To Target
         if (!isCounterToTarget)
         {
@@ -296,52 +343,7 @@ public class PlayerManager : MonoBehaviour
     private void FixedUpdate()
     {
         currentState.FixedUpdateState(transform.gameObject);
-        #region Move
-        if (currentState != revive && !isDead)
-        {
-            Vector3 centerPoint = GameManager.Instance.CenterPoint.position;
-
-            if (GameManager.Instance.state == GameState.BossFight &&
-                GameManager.Instance.curBoss != null && GameManager.Instance.curBoss.activeSelf
-                && UIManager.Instance.isHorizontalMove)
-            {
-                float inputValue = leftInput - rightInput;
-                curMoveX = curMoveX - inputValue * speed * Time.deltaTime;
-
-                if (curMoveX > maxMoveX) curMoveX = maxMoveX;
-                if (curMoveX < minMoveX) curMoveX = minMoveX;
-
-                Vector2 targetPos = new Vector2(centerPoint.x + curMoveX, transform.position.y);
-
-                transform.position = targetPos;
-            }
-            else if (GameManager.Instance.state == GameState.BeforeGameStart)
-            {
-                speed = GameManager.Instance.currentSpeed;
-                Vector2 targetPos = new Vector2(centerPoint.x, transform.position.y);
-                transform.position = Vector3.MoveTowards(transform.position,
-                    targetPos, speed * Time.deltaTime);
-            }
-            else
-            {
-
-                speed = GameManager.Instance.currentSpeed;
-                Vector2 targetPos = new Vector2(centerPoint.x, transform.position.y);
-                if (transform.position.x < targetPos.x)
-                {
-                    transform.position = Vector3.MoveTowards(transform.position,
-                    targetPos, speed * 1.5f * Time.deltaTime);
-                }
-                else
-                {
-                    transform.position = Vector3.MoveTowards(transform.position,
-                    targetPos, speed * Time.deltaTime);
-                }
-
-                curMoveX = .5f;
-            }
-        }
-        #endregion
+       
     }
 
     public void DashPerformed()
