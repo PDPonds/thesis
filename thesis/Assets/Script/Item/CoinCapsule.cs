679121ff23a82e1998c298d14303077106174623
p_fare <newgentype789@hotmail.com>
2024-04-08 13:16:50 +0700

    fixLV4/8

--- thesis/Assets/Script/Item/CoinCapsule.cs
+++ thesis/Assets/Script/Item/CoinCapsule.cs
@@ -6,16 +6,9 @@ public class CoinCapsule : MonoBehaviour
 {
     public int dropCointMin;
     public int dropCoinMax;
-    [SerializeField] GameObject attackInfo;
+    
 
-    private void Update()
-    {
-        float dis = Vector2.Distance(transform.position, PlayerManager.Instance.transform.position);
-        if (dis < 8f)
-            attackInfo.SetActive(true);
-        else
-            attackInfo.SetActive(false);
-    }
+  
 
 
 }
